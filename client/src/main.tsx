5e2e8ba63292670d8c8bd561057aca388b528d9c
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-06-10 12:41:57 +0200

    fix: files name

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,7 +7,7 @@ import { RouterProvider, createBrowserRouter } from "react-router";
 
 // Import the main app component
 import App from "./App";
-import HomePage from "./components/HomePage";
+import HomePage from "./pages/HomePage";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
