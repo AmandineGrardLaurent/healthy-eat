61f2093c47cdddf19cbcb292f039b2e11d9db8a9
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-06-11 10:53:36 +0200

    feat: style css

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,6 +1,8 @@
 import banner from "../assets/images/banner.jpg";
 
 export default function RegisterForm() {
+  const inputStyle = "border-1 border-emerald-600 rounded-sm";
+  const labelStyle = "flex flex-col";
   return (
     <>
       <section className="relative w-full h-screen">
@@ -13,28 +15,28 @@ export default function RegisterForm() {
           Healthy Eat
         </h1>
       </section>
-      <section className="border-b-emerald-700 border-2 m-4">
+      <section className="border-emerald-700 border-2 m-4">
         <h2>Formulaire d'inscription</h2>
-        <form className="flex flex-col">
-          <label>
+        <form className="flex flex-col gap-2 p-7">
+          <label className={labelStyle}>
             Prénom
-            <input />
+            <input className={inputStyle} />
           </label>
-          <label>
+          <label className={labelStyle}>
             Nom
-            <input />
+            <input className={inputStyle} />
           </label>
-          <label>
+          <label className={labelStyle}>
             Pseudo
-            <input />
+            <input className={inputStyle} />
           </label>
-          <label>
+          <label className={labelStyle}>
             Email
-            <input />
+            <input className={inputStyle} />
           </label>
-          <label>
+          <label className={labelStyle}>
             Mot de passe
-            <input />
+            <input className={inputStyle} />
           </label>
           <button type="submit">S'inscrire</button>
         </form>
