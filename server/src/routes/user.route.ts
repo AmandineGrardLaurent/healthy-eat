3eb4e31faf906548b0bb063a13cd350f2d28cc3e
Amandine GRARD-LAURENT <a.grard.laurent08@gmail.com>
2025-06-16 12:27:22 +0200

    feat: validate data register

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -2,17 +2,18 @@ import express from "express";
 import { hashPassword } from "../middlewares/argon.middleware";
 import { checkEmailUnique } from "../middlewares/checkEmailUnique.middleware";
 import { checkPseudoUnique } from "../middlewares/checkPseudoUnique.middleware";
+import { validateDataRegister } from "../middlewares/joi.middleware";
 import { add } from "../modules/user/userActions";
 
 const router = express.Router();
 
 router.post(
   "/api/user",
+  validateDataRegister,
   checkEmailUnique,
   checkPseudoUnique,
   hashPassword,
   add,
 );
-// vérifier les données
 
 export default router;
